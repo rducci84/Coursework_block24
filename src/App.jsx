f7fe763be6c23cc58d5693112c50f4fb513f21b9
Raimundo Ducci <rducci@rdvt-mbp14-macos01.local>
2025-04-17 05:15:39 -0500

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,45 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log("puppyList: ", puppyList);
-  
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    // some logic here
+    {()=>{console.log("puppy id: ", puppy.id)}}
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
-    <>
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => (
+        <p
+          key={puppy.id}
+          onClick={() => {
+            setFeatPupId(puppy.id);
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          {puppy.name}
+        </p>
+      ))}
+
+
+      {featPupId && (
+        <div className="featPupId">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li><strong>Age:</strong> {featuredPup.age}</li>
+            <li><strong>Email:</strong> {featuredPup.email}</li>
+            <li><strong>Owner ID:</strong> {featuredPup.ownerId}</li>
+            <li><strong>Is Cute:</strong> {featuredPup.isCute ? 'Yes' : 'No'}</li>
+            
+          </ul>
+        </div>
+      )}
+
     </div>
-     
-    </>
-  )
+  );
 }
 
 export default App
